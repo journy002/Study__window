c269c26cc307b0cd1b2363066d11503d73d8189d
journy002 <journy002@naver.com>
2021-12-19 01:03:04 +0900

    window load

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // window size 표기[S] //
 
-// const tag = document.querySelector(".tag");
-// function updateTag() {
-//     tag.innerHTML = `
-//     window.screen: ${window.screen.width}, ${window.screen.height}<br/>
-//     window.outer: ${window.outerWidth}, ${window.outerHeight}<br/>
-//     window.inner: ${window.innerWidth}, ${window.innerHeight}<br/>
-//     `;
-// }
+const tag = document.querySelector(".tag");
+function updateTag() {
+    tag.innerHTML = `
+    window.screen: ${window.screen.width}, ${window.screen.height}<br/>
+    window.outer: ${window.outerWidth}, ${window.outerHeight}<br/>
+    window.inner: ${window.innerWidth}, ${window.innerHeight}<br/>
+    `;
+}
 
-// window.addEventListener("resize", () => {
-//     updateTag();
-// });
-// updateTag();
+window.addEventListener("resize", () => {
+    updateTag();
+});
+updateTag();
 
 // window size 표기[E] //
 
