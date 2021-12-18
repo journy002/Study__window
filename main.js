bc2ef9a9cfc3c910eaf4a15e11da5095637cc63f
journy002 <journy002@naver.com>
2021-12-18 18:59:54 +0900

    좌표구하기

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 // window size 표기[S] //
-const tag = document.querySelector(".tag");
-function updateTag() {
-    tag.innerHTML = `
-    window.screen: ${window.screen.width}, ${window.screen.height}<br/>
-    window.outer: ${window.outerWidth}, ${window.outerHeight}<br/>
-    window.inner: ${window.innerWidth}, ${window.innerHeight}<br/>
-    `;
-}
 
-window.addEventListener("resize", () => {
-    updateTag();
-});
-updateTag();
+// const tag = document.querySelector(".tag");
+// function updateTag() {
+//     tag.innerHTML = `
+//     window.screen: ${window.screen.width}, ${window.screen.height}<br/>
+//     window.outer: ${window.outerWidth}, ${window.outerHeight}<br/>
+//     window.inner: ${window.innerWidth}, ${window.innerHeight}<br/>
+//     `;
+// }
+
+// window.addEventListener("resize", () => {
+//     updateTag();
+// });
+// updateTag();
 
 // window size 표기[E] //
 
 // 브라우저 좌표[S] //
+const specialBox = document.querySelector(".box.special");
 
+specialBox.addEventListener("click", (e) => {
+    console.log(`
+    clientX: ${e.clientX}, clientY: ${e.clientY}
+    PageX: ${e.pageX}, pageY: ${e.pageY}
+    `);
+});
 // 브라우저 좌표[E] //
